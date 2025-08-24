f18799ed782fece18f2316d420785a5d8bda3052
suraj-v86 <158526381+suraj-v86@users.noreply.github.com>
2025-08-24 12:22:11 +0200

    Create variables.tf

---
+++ Jenkins_master/variables.tf
@@ -0,0 +1,20 @@
+variable "ami" {
+    description = "value for ami"
+    type = string  
+}
+
+variable "key" {
+    description = "value for key pair"
+    type = string  
+}
+
+variable "instance" {
+    description = "value for instance type"
+    type = string  
+}
+
+variable "subnet" {
+    description = "value for subnet_id"
+    type = string  
+}
+
